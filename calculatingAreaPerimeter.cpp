850ad0054441e3a89999e2d575cb478dd26d180b
Fatma Aktar <106100226+Fatmaaktar@users.noreply.github.com>
2024-01-17 21:40:24 +0300

    Update and rename alanVeÇevreHesaplama.cpp to calculatingAreaPerimeter.cpp

--- calculatingAreaPerimeter.cpp
+++ calculatingAreaPerimeter.cpp
@@ -3,23 +3,26 @@
 #include <conio.h>
 #define pi 3.14
 main(){
-	//karenin alan ve çevre hesaplamasý
+	
+	
 int edge=8;
 printf("karenin cevresi : %d\n" , edge*4);
 printf("karenin alani : %d\n" , edge*8);
 
-	//dikdortgenin alan ve çevre hesaplamasý
+
+	
 int shortEdge=8;
 int longEdge=12;
 
 printf("Dikdorgenin cevresi : %d\n" , shortEdge*2+longEdge*2);
 printf("Dikdorgenin alani : %d\n" , shortEdge+longEdge);
-	//dairenin alan ve çevre hesaplamasý
+	
+	
 int radius=2;
 printf("Dairenin cevresi %f\n" ,2*(pi*radius));
 printf("Dairenin alani %f\n" , pi*(radius*radius));
 
-    //kürenin hacmi
+	
 int r=3;
 printf("kurenin hacmi : %f\n" , 4/3*(pi*r*r*r));
 
